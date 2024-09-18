ac14197d11cfc0655a369e92529b8bd437d07610
kimie100 <hakimie@live.com.my>
2024-09-18 15:59:56 +0800

    vercel2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,14 +8,14 @@ import { AttendsInterface } from "@/types/attendents";
 export const metadata: Metadata = {
     title: "Admin Page",
 };
-const getAllData = async () => {
+// const getAllData = async () => {
 
-    let data: AttendsInterface[] = await db.$queryRaw`SELECT a.userId, u.username,u.name,u.userImg, a.clockIn, a.clockOut,a.img,a.workingHour
-    FROM attends AS a
-    JOIN user AS u ON a.userId = u.id
-    WHERE date(a.clockIn) = CURDATE() OR date(a.clockOut) = CURDATE()`;
-    return data;
-}
+//     let data: AttendsInterface[] = await db.$queryRaw`SELECT a.userId, u.username,u.name,u.userImg, a.clockIn, a.clockOut,a.img,a.workingHour
+//     FROM attends AS a
+//     JOIN user AS u ON a.userId = u.id
+//     WHERE date(a.clockIn) = CURDATE() OR date(a.clockOut) = CURDATE()`;
+//     return data;
+// }
 const Dashboard = async () => {
     // const attends = await getAllData()
     return (
