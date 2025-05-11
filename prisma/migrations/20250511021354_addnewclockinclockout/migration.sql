5a228f504bc866df08eee8cadf3ac202feccba3b
kimie <121069033+kimie100@users.noreply.github.com>
2025-05-11 12:13:47 +0800

    fix sql

--- prisma/migrations/20250511021354_addnewclockinclockout/migration.sql
+++ prisma/migrations/20250511021354_addnewclockinclockout/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE `attendbranch` ADD COLUMN `clockInNew` VARCHAR(191) NULL,
+ALTER TABLE `AttendBranch` ADD COLUMN `clockInNew` VARCHAR(191) NULL,
     ADD COLUMN `clockOutNew` VARCHAR(191) NULL;
--- prisma/migrations/20250511022524_addbranchnew/migration.sql
+++ prisma/migrations/20250511022524_addbranchnew/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `attendbranch` ADD COLUMN `branchNew` VARCHAR(191) NULL;
+ALTER TABLE `AttendBranch` ADD COLUMN `branchNew` VARCHAR(191) NULL;
