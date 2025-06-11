745f64d1a044a49439c69a2fbae561026c60f452
kimie <121069033+kimie100@users.noreply.github.com>
2025-06-11 17:01:28 +0800

    fix branch

--- src/app/api/user/getdata/route.ts
+++ src/app/api/user/getdata/route.ts
@@ -4,7 +4,7 @@ import { db } from "@/lib/db";
 export const POST = async (req: Request) => {
     try {
         const { userId } = await req.json();
-        const users = await db.user.findFirst({ where: { id: userId, role: "USER", isDelete: false }, select: { AttendBranch: { select: { team: true, clockIn: true, clockOut: true } } } })
+        const users = await db.user.findFirst({ where: { id: userId, role: "USER", isDelete: false }, select: { AttendBranch: { select: { team: true, branch: true, clockIn: true, clockOut: true } } } })
         if (!users) return Response.json({ error: "User not found" }, { status: 400 })
         const branchs = await db.branch.findMany({
             where: { team: users.AttendBranch?.team }, select: {
@@ -14,6 +14,7 @@ export const POST = async (req: Request) => {
         let result = {
             clockIn: users.AttendBranch?.clockIn,
             clockOut: users.AttendBranch?.clockOut,
+            userBranch: users.AttendBranch?.branch,
             branchs
         }
         return Response.json(result, { status: 200 })
